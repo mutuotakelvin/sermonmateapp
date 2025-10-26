2a18ec4b173c40d784c6388c6a3d7ec5acfab429
kelvin Mutuota <kelvinmutuota749@gmail.com>
2025-10-26 11:00:52 +0300

    Remove ClerkProvider and update root layout for custom auth

--- app/_layout.native.tsx
+++ app/_layout.native.tsx
@@ -1,36 +1,13 @@
-import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { ElevenLabsProvider } from "@elevenlabs/react-native";
 import { Stack } from "expo-router";
 
-function RootLayoutWithAuth() {
-  const { isSignedIn, isLoaded } = useAuth();
-
-  if (!isLoaded) {
-    return null;
-  }
-
-  return (
-      <ElevenLabsProvider>
-        <Stack >
-            <Stack.Protected guard={isSignedIn}>
-              <Stack.Screen name="(protected)" options={{ headerShown: false, headerTitle: 'Session' }} />
-            </Stack.Protected>
-          <Stack.Protected guard={!isSignedIn}>
-            <Stack.Screen name="(public)" options={{ headerShown: false }} />
-          </Stack.Protected>
-        </Stack>
-      </ElevenLabsProvider>
-  )
-}
-
 export default function RootLayout() {
   return (
-      <ClerkProvider 
-        tokenCache={tokenCache}
-        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      >
-        <RootLayoutWithAuth />
-      </ClerkProvider>
-  )
+    <ElevenLabsProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(public)" options={{ headerShown: false }} />
+        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+      </Stack>
+    </ElevenLabsProvider>
+  );
 }
